12560483c7306b55018b0d711cd7773c284f58c3
Pranjal Sharma <pranjal.ai@outlook.com>
2024-12-19 19:31:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ def format_date(value):
         return value.strftime('%B %d, %Y')
     return value
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
